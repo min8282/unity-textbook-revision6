66987ccdbfd7aa7b49a246706c3e5a824a4cca84
Minseo Jwa <137873062+min8282@users.noreply.github.com>
2024-10-18 23:44:26 +0900

    AppleCatch Final

--- AppleCatch/Assets/BasketController.cs
+++ AppleCatch/Assets/BasketController.cs
@@ -4,27 +4,42 @@ using UnityEngine;
 
 public class BasketController : MonoBehaviour
 {
-    public AudioClip appleSE;
-    public AudioClip bombSE;
+    public AudioClip appleSE;          // 사과 사운드
+    public AudioClip bombSE;           // 폭탄 사운드
+    public GameObject appleParticlePrefab;  // 사과 파티클 프리팹
+    public GameObject bombParticlePrefab;   // 폭탄 파티클 프리팹
     AudioSource aud;
+    GameObject director;
 
     void Start()
     {
         Application.targetFrameRate = 60;
         this.aud = GetComponent<AudioSource>();
+        this.director = GameObject.Find("GameDirector");
     }
 
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Apple")
         {
+            // 사과에 대한 사운드 및 파티클
             this.aud.PlayOneShot(this.appleSE);
+            this.director.GetComponent<GameDirector>().GetApple();
+            
+            // 사과에 대한 파티클 생성
+            Instantiate(appleParticlePrefab, other.transform.position, Quaternion.identity);
         }
-        else
+        else if (other.gameObject.tag == "Bomb")
         {
+            // 폭탄에 대한 사운드 및 파티클
             this.aud.PlayOneShot(this.bombSE);
+            this.director.GetComponent<GameDirector>().GetBomb();
+            
+            // 폭탄에 대한 파티클 생성
+            Instantiate(bombParticlePrefab, other.transform.position, Quaternion.identity);
         }
-        Destroy(other.gameObject);
+
+        Destroy(other.gameObject); // 충돌한 오브젝트 제거
     }
 
     void Update()
